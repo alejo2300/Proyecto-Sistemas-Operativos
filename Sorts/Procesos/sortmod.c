20c8b97b4a59c9b35404b51e33e2c194f1310498
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-09-11 12:55:23 -0500

    Primeras modificaciones del unprog
    Se modifico el unprog y se dio un nombre nuevo a los txt

--- Sorts/Procesos/sortmod.c
+++ Sorts/Procesos/sortmod.c
@@ -3,6 +3,6 @@ int main (int argc, char **argv) {
 /* programa que imprime los argumentos de la l�nea de comandos */ 
 int i; 
   printf("argc = %d\n",argc); 
-  for (i=0;i<argc;++i) 
-    printf("argv[%d]:%s\n",i,argv[i]);
+  for (i=1;i<(argc-1);++i) 
+    printf("El archivo a leer es : %s\n",argv[i]);
 }
