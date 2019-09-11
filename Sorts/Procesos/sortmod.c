9c7bb3ef9e4803b5b65fce368460ba7ed0499da7
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-09-11 12:57:32 -0500

    Cambios en el código de sort con procesos

--- Sorts/Procesos/sortmod.c
+++ Sorts/Procesos/sortmod.c
@@ -1,8 +1,7 @@
 #include<stdio.h> 
 int main (int argc, char **argv) { 
-/* programa que imprime los argumentos de la l�nea de comandos */ 
-int i; 
-  printf("argc = %d\n",argc); 
+//Organizacion de archivos en un n archivo por medio de procesos
+int i;  
   for (i=1;i<(argc-1);++i) 
     printf("El archivo a leer es : %s\n",argv[i]);
 }
