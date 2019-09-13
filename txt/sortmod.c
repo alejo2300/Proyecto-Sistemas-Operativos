29a6229ecc412d101ede98c71454cbd9978b510b
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-09-13 18:28:08 -0500

    Errores al leer
    Tengo problemas con la lectura del txt, la ultima linea se repite

--- txt/sortmod.c
+++ txt/sortmod.c
@@ -4,19 +4,19 @@
 int main (int argc, char **argv) {
 //Organizacion de archivos en un n archivo por medio de procesos
     int i;
-    FILE *f;
+    FILE *archivo;
+    int pos=0;
     for (i=1;i<(argc-1);++i){
         printf("El archivo a leer es : %s\n",argv[i]);
-        f=fopen(argv[i],"rb");
-        char cadena[100],sbcadena[50];
-        while(feof(f)==0){
-            fscanf(f,"%s",&cadena);
-            fscanf(f,"%s",&sbcadena);
-            strcat(cadena," ");
-            strcat(cadena,sbcadena);
-            printf("%s\n",cadena);
+        archivo=fopen(argv[i],"r");
+        char cline[100];
+        while(!feof(archivo)){
+            fgets(cline,100,archivo);
+            pos++;
+            printf("%s",cline);
+
         }
-        if(f==NULL){
+        if(archivo==NULL){
             printf("Error al abrir el archivo %s\n",argv[i]);
         }
     }
